31b2831c05ba69e9df64438d6c5a6aa3fc44ff17
Dave <40533786+iamsurelydaveydave@users.noreply.github.com>
2025-05-16 14:20:08 +0800

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,8 @@
 import vuetify, { transformAssetUrls } from "vite-plugin-vuetify";
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: "2024-09-30",
+  compatibilityDate: "2025-02-20",
+  ssr: false,
   devtools: { enabled: true },
   css: ["@/assets/main.css"],
   build: {
